525919de38dbae3ae0be4c459a622e97e3fcb279
mr-fact <mr.fact.202020@gmail.com>
2023-10-12 11:05:46 +0330

    create logs.py

---
+++ mini_4_view/mini_4_view/logs.py
@@ -0,0 +1,26 @@
+
+
+def start_end_log(func):
+    def inner(self, *args, **kwargs):
+        log_message = getattr(func, 'log_message', '')
+        log_level = getattr(func, 'log_level', 0)
+        log_level_spaces = log_level * '  '
+        print(f'{log_level_spaces}+++[{func.__name__} - {log_message}]')
+        result = func(self, *args, **kwargs)
+        print(f'{log_level_spaces}---[{func.__name__} - {log_message}]')
+        return result
+    return inner
+
+
+def message_log(message):
+    def inner(func):
+        func.log_message = message
+        return func
+    return inner
+
+
+def level_log(level):
+    def inner(func):
+        func.log_level = level
+        return func
+    return inner
